a2f26d928699965469e491d430082aadb0a03684
Ryan Bak <rbak@cray.com>
2021-02-10 09:18:33 -0700

    CASMCMS-6678: Fix inventory container image to not use latest

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -8,4 +8,6 @@
 wget https://arti.dev.cray.com/artifactory/csm-misc-master-local/manifest/manifest.txt
 aee_image_tag=$(cat manifest.txt | grep cray-aee | sed s/.*://g | tr -d '[:space:]')
 sed -i s/@aee_image_tag@/${aee_image_tag}/g kubernetes/cray-cfs-operator/values.yaml
+cfs_operator_image_tag=$(cat .version)
+sed -i s/@cfs_operator_image_tag@/${cfs_operator_image_tag}/g kubernetes/cray-cfs-operator/values.yaml
 rm manifest.txt
