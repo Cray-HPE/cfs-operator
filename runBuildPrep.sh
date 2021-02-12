c052bfa3869b81b7b1789a7f5039db9748e31670
Ryan Bak <rbak@cray.com>
2021-02-12 14:31:35 -0700

    CASMCMS-6678: Update to use update_tags script for dependecies

--- runBuildPrep.sh
+++ runBuildPrep.sh
@@ -4,10 +4,4 @@
 # Copyright 2020-2021 Hewlett Packard Enterprise Development LP
 #
 
-# The URL to the manifest.txt file must be updated to point to the stable manifest when cutting a release branch.
-wget https://arti.dev.cray.com/artifactory/csm-misc-master-local/manifest/manifest.txt
-aee_image_tag=$(cat manifest.txt | grep cray-aee | sed s/.*://g | tr -d '[:space:]')
-sed -i s/@aee_image_tag@/${aee_image_tag}/g kubernetes/cray-cfs-operator/values.yaml
-cfs_operator_image_tag=$(cat .version)
-sed -i s/@cfs_operator_image_tag@/${cfs_operator_image_tag}/g kubernetes/cray-cfs-operator/values.yaml
-rm manifest.txt
+./update_tags.sh
