1e5754d3c991be15d388fefd67ba68d9792c533c
Ryan Bak <rbak@cray.com>
2022-01-25 13:58:48 -0700

    CASMCMS-7784: Add ansible passthrough parameter

--- src/cray/cfs/operator/events/session_events.py
+++ src/cray/cfs/operator/events/session_events.py
@@ -1,4 +1,7 @@
-# Copyright 2019-2022 Hewlett Packard Enterprise Development LP
+#
+# MIT License
+#
+# (C) Copyright 2019-2022 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -12,19 +15,19 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-#
-# (MIT License)
+
 """
 Functions for handling CFS Session Events
 """
 import json
 import logging
 import os
+import shlex
 import time
 import threading
 import uuid
@@ -420,6 +423,9 @@ class CFSSessionController:
         ansible_containers = []
         ansible_config = options.default_ansible_config
         ansible_args = []
+        disable_state_recording=False
+        if session_data['target']['definition'] == 'image':
+            disable_state_recording=True
         if 'ansible' in session_data:
             ansible_spec = session_data['ansible']
             ansible_config = ansible_spec.get('config', ansible_config)
@@ -436,6 +442,11 @@ class CFSSessionController:
             if limit:
                 ansible_args.append('--limit')
                 ansible_args.append(limit)
+
+            ansible_passthrough = ansible_spec.get('passthrough', '')
+            if ansible_passthrough:
+                ansible_args.extend(shlex.split(ansible_passthrough, posix=False))
+                disable_state_recording=True
 
         previous = ''
         for i, layer in configuration:
@@ -480,6 +491,10 @@ class CFSSessionController:
                     client.V1EnvVar(
                         name='INVENTORY_TYPE',
                         value=session_data['target']['definition']
+                    ),
+                    client.V1EnvVar(
+                        name='DISABLE_STATE_RECORDING',
+                        value=str(disable_state_recording)
                     )
                 ],  # env
                 volume_mounts=[
