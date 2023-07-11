df51367beb1216b05ce144bf963038f03b686997
Ryan Bak <rbak@cray.com>
2023-07-11 13:25:19 -0400

    CASMCMS-7077: Add support for special debug configurations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,6 +13,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 - Added debugging wait time for failed sessions
+- Added support for special debug playbooks
 
 ## [1.18.3] - 7/25/2023
 ### Dependencies
--- src/cray/cfs/operator/events/session_events.py
+++ src/cray/cfs/operator/events/session_events.py
@@ -435,6 +435,8 @@ class CFSSessionController:
         disable_state_recording=False
         if session_data['target']['definition'] == 'image':
             disable_state_recording=True
+        if len(configuration) == 1 and configuration[0][0] == "debug":
+            disable_state_recording=True
         if 'ansible' in session_data:
             ansible_spec = session_data['ansible']
             ansible_config = ansible_spec.get('config', ansible_config)
@@ -488,7 +490,7 @@ class CFSSessionController:
                 ),
                 client.V1EnvVar(
                     name='DEBUG_WAIT_TIME',
-                    value=debug_wait_time
+                    value=str(debug_wait_time)
                 )
             ],  # env
             volume_mounts=[
@@ -527,14 +529,27 @@ class CFSSessionController:
             args=teardown_args,
         )  # V1Container
 
-    def _create_k8s_job(self, session_data, job_id):  # noqa: C901
-        """
-        When a CFS Session is created, kick off the k8s job.
-        """
-        options.update()
 
+    def _get_debug_configuration_data(self, configuration_name):
+        additional_inventory = {}
+        git_configuration_data = []
+        debug_configuration_data = {
+            "clone_url": "",
+            "playbook": f"{configuration_name[len('debug_'):]}.yaml",
+            "layer": "_debug"
+        }
+        ansible_configuration_data = [("debug", debug_configuration_data)]
+        return git_configuration_data, ansible_configuration_data, additional_inventory
+
+
+    def _get_configuration_data(self, session_data):
         configuration_name = session_data['configuration']['name']
-        cfs_config = get_configuration(configuration_name)
+        try:
+            cfs_config = get_configuration(configuration_name)
+        except Exception as e:
+            if configuration_name.startswith("debug_"):
+                return self._get_debug_configuration_data(configuration_name)
+            raise e
         configuration = cfs_config.get('layers', [])
         configuration_limit = session_data['configuration'].get('limit', '')
         additional_inventory = cfs_config.get('additional_inventory', {})
@@ -552,6 +567,16 @@ class CFSSessionController:
             configuration = [(str(i), layer) for i, layer in enumerate(configuration)]
         for i, layer in configuration:
             layer["layer"] = i
+        return configuration, configuration, additional_inventory
+
+
+    def _create_k8s_job(self, session_data, job_id):  # noqa: C901
+        """
+        When a CFS Session is created, kick off the k8s job.
+        """
+        options.update()
+
+        git_configuration_data, ansible_configuration_data, additional_inventory = self._get_configuration_data(session_data)
 
         ansible_config = options.default_ansible_config
         if 'ansible' in session_data:
@@ -567,13 +592,13 @@ class CFSSessionController:
         if options.additional_inventory_url and not additional_inventory:
             additional_inventory = {'clone_url': options.additional_inventory_url,
                                     'commit': 'master'}
-        clone_container = self._get_clone_container(configuration, additional_inventory)
+        clone_container = self._get_clone_container(git_configuration_data, additional_inventory)
 
         # Inventory container
         inventory_container = self._get_inventory_container(session_data)
 
         # Ansible containers
-        ansible_container = self._get_ansible_container(session_data, configuration)
+        ansible_container = self._get_ansible_container(session_data, ansible_configuration_data)
 
         # Assemble the containers, if this is image customization, add the IMS
         # teardown containers to the list
