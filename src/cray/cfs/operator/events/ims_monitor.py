86a17cf7f58c336adf0bb52981bd4a779996d064
David Laine <laine@hpe.com>
2024-10-17 11:14:33 -0500

    More log output.

--- src/cray/cfs/operator/events/ims_monitor.py
+++ src/cray/cfs/operator/events/ims_monitor.py
@@ -52,6 +52,8 @@ class IMSJobMonitor:
     @staticmethod
     def _get_running_ims_jobs():
         jobs = get_ims_jobs()
+        for job in jobs:
+            LOGGER.info(f"Job: {job.get("id")}, status: {job.get("status")}")
         return [job["id"] for job in jobs if job.get("status") not in ["success", "error"]]
 
     @staticmethod
